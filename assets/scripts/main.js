369423beaed7971712b552a140369d4451ec2b6d
alex <alexl@advanstaff.com>
2024-11-30 17:25:57 -0700

    Got links and previews working

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,32 +1,58 @@
 // https://raw.githubusercontent.com/alex0112/dotfiles/refs/heads/master/.bashrc
 
-function validateGithubURL(event) {
+function submitButtonHandler(event) {
   const urlInput = document.querySelector("#url-input");
+  const url = urlInput.value;
+  const errorDisplay = document.querySelector("#error-display");
+  errorDisplay.textContent = ""; // Clear the error state in case it's populated
 
-  console.log(`If this were a real validator I would validate this url: ${urlInput.value}`)
+  const validURL = validateGithubURL(url);
 
-  updateCodeBlock(urlInput.value);
-
+  if (validURL) {
+    fetchGithubURL(url);
+  } else {
+    errorDisplay.textContent = "Please enter a valid Github File URL.";
+    setTimeout(() => { // set a timeout callback to clear the error and the input after a bit
+      urlInput.value = "";
+      errorDisplay.textContent = "";
+    }, 5000);
+  }
 }
 
-function urlFetchHandler(event) {
-  
+function fetchGithubURL(url) {
+  ////////////////////////////////////
+  // Invokes the code block update. //
+  ////////////////////////////////////
+
+  const rawURL = generateRawURL(url);
+  updateCodeBlock(rawURL);
 }
 
-async function fetchGithubRaw(url) {
-  const resp = await fetch(url);
+function validateGithubURL(url) {
+  const githubMatcher = /[https:\/\/]*github.com\/.*\/blob\/.*/;
 
-  
+  return githubMatcher.test(url);
+}
+
+// https://github.com/alex0112/yggdrasil/blob/master/public/index.js
+// https://raw.githubusercontent.com/alex0112/yggdrasil/refs/heads/master/public/index.js
+function generateRawURL(url) {
+  url = url.replace("blob", "refs/heads");
+  url = url.replace("github.com", "raw.githubusercontent.com");
+
+  return url;
 }
 
 function updateCodeBlock(url) {
-  let loader    = document.querySelector("#loader");;
-  let codeBlock = document.querySelector("#code > pre > code");
+  const loader    = document.querySelector("#loader");;
+  const codeBlock = document.querySelector("#code > pre > code");
 
+  // set the loader while we're waiting:
   loader.style.display = "block";
   codeBlock.style.display = "none";
 
-  fetch(url)
+  // fetch the URL
+  fetch(url, {redirect: "follow"})
     .then(response => response.text())
     .then(data => {
 
@@ -35,14 +61,13 @@ function updateCodeBlock(url) {
 
       // Update the codeblock with the content of the github source
       codeBlock.textContent = data;
-      codeBlock.removeAttribute("data-highlighted"); // It needs to remove this or we don't get any new highlighted content
-      console.log(`Got ${data} back from the fetcher`);
+      codeBlock.removeAttribute("data-highlighted"); // It needs to remove this or we don't get any new highlighted content from hl.js
+      // console.log(`Got ${data} back from the fetcher`);
       hljs.highlightAll();
     })
     .catch(error => console.error('Error:', error));
 }
 
-// let test_url = "https://raw.githubusercontent.com/git/git/refs/heads/master/grep.c";
-let test_url = "https://raw.githubusercontent.com/alex0112/prxs/refs/heads/main/src/input_state.rs";
-
-document.addEventListener('DOMContentLoaded', () => updateCodeBlock(test_url))
+// Update the code block with some initial text
+const initial_url = "https://raw.githubusercontent.com/alex0112/prxs/refs/heads/main/src/input_state.rs";
+document.addEventListener('DOMContentLoaded', () => updateCodeBlock(initial_url));
